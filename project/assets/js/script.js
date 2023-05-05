bbf03a0c21daa2139ea6457ce28185f43e3b151a
Rajiv <rajivsraj78@gmail.com>
2023-05-05 17:36:49 +0530

    Media query done

---
+++ project/assets/js/script.js
@@ -0,0 +1,9 @@
+// var x = document.getElementById("menuid");
+
+// console.log(x);
+
+function aarti(){
+    var x = document.getElementById("m");
+    console.log(x);
+    x.style.display = "block";
+}
